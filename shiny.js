1376143057c23ea627d318c04e3658c30d985be4
changwoolim <limcw@zarathu.com>
2023-08-05 11:54:58 +0900

    Update shiny.js

--- shiny.js
+++ shiny.js
@@ -81,33 +81,8 @@ import("https://webr.r-wasm.org/latest/webr.mjs").then(async ({ WebR }) => {
 	})();
 
 	// Upload file to webR filesystem
-	async function fetchToWebR(url, path) {
+	async function fetchToWebR(code, path) {
 		// const req = await fetch(url);
-		const code = `
-    # Define server logic required to draw a histogram ----
-server <- function(input, output) {
-
-  # Histogram of the Old Faithful Geyser Data ----
-  # with requested number of bins
-  # This expression that generates a histogram is wrapped in a call
-  # to renderPlot to indicate that:
-  #
-  # 1. It is "reactive" and therefore should be automatically
-  #    re-executed when inputs (input$bins) change
-  # 2. Its output type is a plot
-  output$distPlot <- renderPlot({
-
-    x    <- faithful$waiting
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-    hist(x, breaks = bins, col = "#75AADB", border = "white",
-         xlab = "Wadddditing time to next eruption (in mins)",
-         main = "Histogram of waiting times")
-
-    })
-
-}
-`;
 		const uint8ArrayCode = new Uint8Array([...code].map((char) => char.charCodeAt(0)));
 		await webR.FS.writeFile(path, uint8ArrayCode);
 		// const data = await req.arrayBuf fer();
@@ -125,8 +100,70 @@ server <- function(input, output) {
 	// Setup shiny app on webR VFS
 	await webR.FS.mkdir("/home/web_user/app");
 	await webR.FS.mkdir("/home/web_user/app/www");
-	await fetchToWebR("app/ui.R", "/home/web_user/app/ui.R");
-	await fetchToWebR("app/server.R", "/home/web_user/app/server.R");
+	await fetchToWebR(
+		`# Define UI for app that draws a histogram ----
+  ui <- fluidPage(
+  
+    # App title ----
+    titlePanel("Hello Shiny!"),
+  
+    # Sidebar layout with input and output definitions ----
+    sidebarLayout(
+  
+      # Sidebar panel for inputs ----
+      sidebarPanel(
+  
+        # Input: Slider for the number of bins ----
+        sliderInput(inputId = "bins",
+                    label = "Number of bins:",
+                    min = 1,
+                    max = 50,
+                    value = 30)
+  
+      ),
+  
+      # Main panel for displaying outputs ----
+      mainPanel(
+  
+        # Output: Histogram ----
+        plotOutput(outputId = "distPlot")
+  
+      )
+    )
+  )
+  `,
+		//"app/ui.R",
+		"/home/web_user/app/ui.R"
+	);
+	await fetchToWebR(
+		`
+  # Define server logic required to draw a histogram ----
+server <- function(input, output) {
+
+# Histogram of the Old Faithful Geyser Data ----
+# with requested number of bins
+# This expression that generates a histogram is wrapped in a call
+# to renderPlot to indicate that:
+#
+# 1. It is "reactive" and therefore should be automatically
+#    re-executed when inputs (input$bins) change
+# 2. Its output type is a plot
+output$distPlot <- renderPlot({
+
+  x    <- faithful$waiting
+  bins <- seq(min(x), max(x), length.out = input$bins + 1)
+
+  hist(x, breaks = bins, col = "#75AADB", border = "white",
+       xlab = "Wadddditing time to next eruption (in mins)",
+       main = "Histogram of waiting times")
+
+  })
+
+}
+`,
+		//  "app/server.R",
+		"/home/web_user/app/server.R"
+	);
 
 	// Install and run shiny
 	await webR.evalRVoid(`webr::install("shiny", repos="${window.location.href}/repo/")`);
